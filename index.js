516d0dbdcbdb35c3a9ad02af50cdea2dd10c53ce
retr0 <395637649@qq.com>
2019-06-18 09:47:46 +0800

    finishing work

--- index.js
+++ index.js
@@ -4,13 +4,27 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+name = name.toUpperCase();
+console.log(name);
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+var temp = sentence.split(" ");
+sentence = "";
+for (let i = 0;i < temp.length;i++) {
+    if (i == 0) {
+    sentence = sentence + temp[i].charAt(0).toUpperCase() + temp[i].substr(1);
+    }
+    else 
+    sentence = sentence + " " + temp[i].charAt(0).toUpperCase() + temp[i].substr(1);
+}
+console.log(sentence);
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+money = parseInt(money.substr(1));
+console.log(money);
